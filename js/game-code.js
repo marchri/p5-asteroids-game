1cb26d327baa00fb7adba50026550695eb59e495
marchri <124294311+marchri@users.noreply.github.com>
2023-02-06 10:26:22 +0200

    Update game-code.js

--- js/game-code.js
+++ js/game-code.js
@@ -505,7 +505,7 @@
 			textSize(30);
 			text("Score: " + asteroidSwarm.asteroidsPassed, 30, 135); // Score is shown
 			
-			text("Missiles: " + spaceship.missiles, 1120, 60); // Score is shown
+			text("BOBES: " + spaceship.missiles, 1120, 60); // Score is shown
 		
 			if (!startGame)
 				rect(280, 280, 680, 140); // rectangle (window) to show the message to start game
@@ -516,10 +516,10 @@
 			textSize(50);
 			if (!startGame || gameOver) // provide instructions
 			{
-				text('Press N to start a new game.', 300, 300, 800, 200);
+				text('Press N to start a new game^-^.', 300, 300, 800, 200);
 				textSize(25);
-				text('Use the left and right arrows to avoid the asteroids.', 340, 360, 800, 200);
-				text('Get the missiles and fire by pressing the Space bar!', 340, 390, 800, 200);
+				text('Use the left and right arrows to avoid the TAPIOKA PERALS.', 340, 360, 800, 200);
+				text('Get the sponges and fire by pressing the Space bar!', 340, 390, 800, 200);
 			}
 			
 			if (gameOver)
